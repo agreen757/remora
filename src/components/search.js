8c2df4c4aedfeaf8abf647fd769618b718ba5238
Adrian Green <dominion.green@gmail.com>
2023-10-20 17:31:02 +0000

    removing results number input

--- src/components/search.js
+++ src/components/search.js
@@ -1,55 +1,44 @@
 //React component will be the search input
 
-import React, { useState,useEffect } from "react";
-import { Box, Button, Text, Image, Stack, Input } from "@chakra-ui/react"
+import React, { useState, useEffect } from "react";
+import { Box, Button, Text, Image, Stack, Input } from "@chakra-ui/react";
 
 export default function Search(props) {
-    const handleChange = props.handleChange;
-    const handleResultsChange = props.handleResultsChange;
-    const searchloc = props.searchloc;
+  const handleChange = props.handleChange;
+  const handleResultsChange = props.handleResultsChange;
+  const searchloc = props.searchloc;
 
-    return (
-        <Box pt="10" height="140px">
-        <Stack direction={'row'} spacing={3}>
-          <Box>
+  return (
+    <Box pt="10" height="140px">
+      <Stack direction={"row"} spacing={3}>
+        <Box>
           <Text>Search Location</Text>
           <Box>
-            <Stack direction={'row'}>
+            <Stack direction={"row"}>
               <Box>
-              <Input
-                id="search"
-                border={'1px solid white'}
-                input=""
-                onChange={handleChange}
-                placeholder="Search Location"
-              />
-              </Box>
-              <Box maxW={'70px'}>
-              <Input
-                  border={'1px solid white'}
-                  id="results"
+                <Input
+                  id="search"
+                  border={"1px solid white"}
                   input=""
-                  type="number"
-                  onChange={handleResultsChange}
-                  placeholder="#"
+                  onChange={handleChange}
+                  placeholder="Search Location"
                 />
               </Box>
             </Stack>
           </Box>
-          </Box>
-        </Stack>
-         <Box pt={'13px'} >
-            <Button
-                size={'xs'}
-                border={'1px solid white'}
-                onClick={() => {
-                  searchloc();
-                }}
-              >
-                Confirm
-            </Button>
-          </Box>
+        </Box>
+      </Stack>
+      <Box pt={"13px"}>
+        <Button
+          size={"xs"}
+          border={"1px solid white"}
+          onClick={() => {
+            searchloc();
+          }}
+        >
+          Confirm
+        </Button>
       </Box>
-    )
-
+    </Box>
+  );
 }
